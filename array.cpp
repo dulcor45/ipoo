2e2d8dacca92d46a1471ac1878e5d59ee33c398c
juanvm97 <juan.velasquez@ucsp.edu.pe>
2015-09-01 15:32:37 -0500

    Ejercicio con array

---
+++ array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int a[10], b[10], c[20];
+
+    for (int i=0; i<20; i++){
+
+        if (i<10){
+            cout << "numero: "; cin >> a[i];
+            c[i]=a[i];
+            }
+
+        else {
+            cout << "numero: "; cin >> b[i-10];
+            c[i]=b[i-10];
+            }
+        }
+
+    for (int i=0; i<20; i++){
+
+        cout << c[i]<< endl;
+        }
+
+    return 0;
+}
