7b96a9df5775285d73abbe910d813c6659a017b1
juanvm97 <juan.velasquez@ucsp.edu.pe>
2015-12-03 07:52:34 -0500

    clases

---
+++ Clases 21-09/Punto.cpp
@@ -0,0 +1,32 @@
+#include "Punto.h"
+#include <iostream>
+
+using namespace std;
+
+Punto::Punto()
+{
+	x = 0.0;
+	y = 0.0;
+}
+
+Punto::Punto(double _X, double _Y)
+{
+		x = _X;
+		y = _Y;
+}
+
+double Punto::Get_X()
+{
+	return x;
+}
+
+double Punto::Get_Y()
+{
+	return y;
+}
+
+void Punto::Print()
+{
+	cout << "(" << Get_X() << ";" << Get_Y() << ")" << endl;
+}
+
---
+++ Clases 21-09/Vector.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+
+#include "Vector.h"
+#include <cmath>
+
+using namespace std;
+
+Vector::Vector(){}
+
+Vector::Vector(double X1, double Y1, double X2, double Y2){
+	
+	Punto _Start(X1, Y1), _End(X2, Y2);	
+	Start = _Start;
+	End = _End;
+
+}
+
+double Vector::Distance(){
+	return sqrt(pow((End.Get_X() - Start.Get_X()),2) + pow((End.Get_Y() - Start.Get_Y()),2));
+}
+
+void Vector::Print()
+{
+	cout << endl;
+	cout << "   Las cordenadas en el primer punto son: (" << Start.Get_X() << " ; " << Start.Get_Y() << ")" << endl;
+	cout << endl;
+	cout << "   Las cordenadas en el segundo punto son: (" << End.Get_X() << " ; " << End.Get_Y() << ")" << endl;
+	cout << endl;
+	
+}
+
+
+
+
