ab9ef90ca39bbe21545b6a00399725baf81e8dd0
juanvm97 <juan.velasquez@ucsp.edu.pe>
2015-12-03 00:34:37 -0500

    clases

---
+++ Clase 02-10/Punto.h
@@ -0,0 +1,18 @@
+
+
+class Punto
+{
+private:
+	double x, y;
+
+public:
+
+	Punto();
+	Punto(double _X, double _Y);
+	double Get_X();
+	double Get_Y();
+	void Print();
+	~Punto();
+
+};
+
---
+++ Clase 02-10/main.cpp
@@ -0,0 +1,19 @@
+
+#include "Vector.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	if (true)
+	{
+	Vector V1(2.4, 5.3, 3.1, 8.9);
+
+	V1.Print();
+	cout << "  La distancia es" << V1.Distance() << endl;
+	}
+
+	return 0;
+}
